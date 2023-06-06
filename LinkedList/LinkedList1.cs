3e1e8a8333d9b862b96e6e7db7fe7690e5bf3aa4
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-06-06 16:25:15 +0530

    adding

---
+++ LinkedList/LinkedList1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList1
+    {
+        private Node Head;
+
+        public void AddLast(int data)
+        {
+            Node newNode=new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                Console.WriteLine("{0} is add  to the LinkedList", data);
+            }
+            else
+            {
+                Node lastnode= GetLastNode();
+                lastnode.Next = newNode;
+                Console.WriteLine("{0} is add  to the LinkedList", data);
+                
+            }
+        }
+        private Node GetLastNode()
+        {
+            Node temp = Head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            return temp;
+        }
+        public void Display()
+        {
+            Node current = Head;
+            while(current.Next != null)
+            {
+                //Console.WriteLine(current.Data +"  ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList1 LinkedList=new LinkedList1();
+            LinkedList.AddLast(56);
+            LinkedList.AddLast(30);
+            LinkedList.AddLast(70);
+           
+
+
+            LinkedList.Display();
+
+
+
+
+            
+          
+              
+
+        }
+    }
+}
